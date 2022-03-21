e405b24ed2a1cc28eb30c2a47ad568bddd96d9a9
Nghiem Tung Duong <nghiemduong2000@Nghiems-MacBook-Pro.local>
2022-03-21 17:35:10 +0700

    feat: multiple function

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const sum = (num1, num2) => {
   return num1 + num2;
 }
 
+const multiple = (num1, num2) => {
+  return num1 + num2;
+}
+
 console.log(one, two);
 console.log(two, one);
 console.log(sum(one, two));
